a82422a1caac01e7d304b94c049d191f0bde1486
imanoldt <imanol.duran@opendeusto.es>
2022-10-25 13:21:36 +0200

    Creacion de modelos

--- app_1/models.py
+++ app_1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Usuario(models.Model):
+    name = models.CharField(max_lenght=20)
+    edad = models.IntegerField())
+    contrasenya = forms.CharField(widget=forms.PasswordInput)
+
