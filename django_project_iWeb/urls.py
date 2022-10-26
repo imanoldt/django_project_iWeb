17b37effe43afb7ddc2b19f5ce8a85e5116cc99e
IreeneGG <91026233+IreeneGG@users.noreply.github.com>
2022-10-26 16:36:41 +0200

    Arreglo Template

--- django_project_iWeb/urls.py
+++ django_project_iWeb/urls.py
@@ -21,6 +21,11 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('',TemplateView.as_view(template_name="app_1/index.html")),
+    path('app_1/', include('app_1.urls')),
+   # path('',TemplateView.as_view(template_name="app_1/index.html")),
+   # path('',TemplateView.as_view(template_name="Login-Bootstrap-5/index.html")),
+   # path('accounts/login/',TemplateView.as_view(template_name="Login-Bootstrap-5/index.html")),
+    
+    
 ]
 
