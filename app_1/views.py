f9ac3a04de951db5a3d9d182b95a713b337208a6
Imanol Duran Tejada <90787303+imanoldt@users.noreply.github.com>
2022-10-26 23:51:31 +0200

    Redireccionamiento de Paginas

--- app_1/views.py
+++ app_1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,"Login-Bootstrap-5/index.html")
+
+def sign_up(request):
+    return render(request,"Login-Bootstrap-5/sign_up.html")
