d1171ec8362e522b5e35ae7ec9036cf19fa0871e
Imanol Duran Tejada <imanol.duran@opendeusto.es>
2022-10-27 16:24:03 +0200

    Rediseño Componentes&Interfaz
    - Diseño de logos
    - Diseño de imagenes
    - Interfaz

--- app_1/views.py
+++ app_1/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+PUBLIC_DOMAIN_NAME = '127.0.0.1:8000'
 # Create your views here.
 def login(request):
-    return render(request,"Login-Bootstrap-5/index.html")
+    return render(request, "Login-Bootstrap-5/index.html")
+
 
 def sign_up(request):
-    return render(request,"Login-Bootstrap-5/sign_up.html")
+    return render(request, "Login-Bootstrap-5/sign_up.html")
