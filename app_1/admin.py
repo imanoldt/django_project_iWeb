69311770511f86e76404f450f45d5780218a7c4c
Imanol Duran Tejada <90787303+imanoldt@users.noreply.github.com>
2022-11-12 17:41:44 +0100

    Modificaciones Avanzadas & more.
    Se han realizado los siguientes cambios:
    
    admin.py:
    - Cambio implementacion de mdelos como habiamos visto.
    - Se han cambiado algunos parametros de algunos modelos como video.
    - Se han añadido algunos metodos para un mejor manejo en la pestaña ADMIN
    
    models.py:
    
    - Video permite la posibilidad de categorizar los videos (Ocio,Entretenimiento,...)
    - Video añade la posibilidad de añadir una miniatura para poder modificar su imagen en la vista Video
    
    Templates
    
    - Se ha modificado los iconos del nav para una navegacion mas optima
    - Se ha cambiado el estido de la pagina Videos.html

--- app_1/admin.py
+++ app_1/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
-from .models import User, Video, Channel
+from .models import User, Video, Channel, Comment
 
 
-admin.site.register(User)#MODELS.py
-admin.site.register(Video) #MODELS.py
-admin.site.register(Channel)#MODELS.py
+#MODIFICADO 
+
+""" 
+La administracion de los modelos se convierten en clases
+Administracion del panel admin
+Cambios en el listado de Videos
+ """
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'contrasenya')
+    ordering = ('id', 'name')
+    search_fields = ('id', 'name')
+
+@admin.register(Video)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'path', 'thumnail', 'cathegory', 'length')
+    list_editable = ('title', 'cathegory',)
+    list_filter = ('cathegory',)
+    ordering = ('id', 'title')
+    search_fields = ('id', 'title')
+
+@admin.register(Channel)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'channel_name', 'subscribers')
+
+@admin.register(Comment)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+
+""" admin.site.register(User)
+admin.site.register(Video) 
+admin.site.register(Channel)"""
--- app_1/models.py
+++ app_1/models.py
@@ -2,21 +2,39 @@ from django.db import models
 from django import forms
 
 
-class User(models.Model):
+class User(models.Model):#MODIFICADO //FALTA IMPLEMENTARLO
     name = models.CharField(max_length=10)
-    edad = models.IntegerField()
+    #CAMPO EDAD BORRADO (DUDO QUE HAGA FALTA) //FALTA IMPLEMENTARLO
     contrasenya = forms.CharField(widget=forms.PasswordInput)
 
-
-class Video(models.Model):
+VIDEO_CATHEGORY =(
+    ("1", "COMIDA"),
+    ("2", "DEPORTE"),
+    ("3", "ENTRETENIMIENTO"),
+    ("4", "OCIO"),
+    ("5", "SERIE"),
+    ("6", "VIAJES"),
+    ("7", "BUSINESS"),
+)
+class Video(models.Model): #MODIFICADO
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=300, null=True)
     path = models.CharField(max_length=2000, null=True)
+    thumnail = models.CharField(max_length=2000, null=True) #MINIATURA DE IMAGEN PARA MEJOR OPTIMIZACION DE LA PAGINA
+    length = models.IntegerField(null=True) #DURACION DEL VIDEO ENTERO
     datetime = models.DateTimeField(auto_now=True, blank=False, null=False)
+    cathegory = models.CharField(max_length=300, null=True, choices = VIDEO_CATHEGORY)
     #user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    def __str__(self): #DEVUELVE STRING DE LO QUE QUEREMOS VISUALIZAR / TOSTRING() _ JAVA
+        return self.title
+        
 
 
 class Channel(models.Model):
     channel_name = models.CharField(max_length=50, blank=False, null=False)
     subscribers = models.IntegerField(default=0, blank=False, null=False)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+class Comment(models.Model):#MODIFICADO
+ title = models.CharField(max_length=50, blank=False, null=False)
