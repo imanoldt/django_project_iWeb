ff38abfd8f3cc8ead1ee2ab31b5c05ba2f6930e2
Imanol Duran Tejada <90787303+imanoldt@users.noreply.github.com>
2022-11-22 00:00:36 +0100

    Modificaciones / Views Stadistic
    - Diseño nuevo en profile igual que videos

--- app_1/static/js/grafica.js
+++ app_1/static/js/grafica.js
@@ -1,43 +1,21 @@
-Chart.defaults.color='#fff'
-Chart.defaults.fontSize=40 /*NO LO ESTA COGIENDO */
 
 
+const ctx_e = document.getElementById("myChart").getContext("2d");
 
-const getColors = opacity => {
-  const colors = ['#7448c2','#d99e2b']
-  return colors.map(color => opacity ? `${color + opacity}` : color)
-}
-
-const ctx = document.getElementById('myChart').getContext("2d");   
-new Chart(ctx, {
-type: 'pie',
-data: {
-  labels: ['LIKES', 'DISLIKES'],
-  datasets: [
+new Chart(ctx_e, {
+  type: "pie",
+  data: {
+    labels: ["LIKES", "DISLIKES"],
+    datasets: [
       {
-          label: 'ESTADISTICA',
-          data: [document.getElementById("likes").innerHTML, document.getElementById("dislikes").innerHTML],
-          borderColor: getColors(),
-          backgroundColor:getColors(20),
-        },  
-  ]
-
-},
-options: {
-  scales: {
-      yAxes:[{
-          ticks:{
-              beginAtZero:true
-              
-          }
-      }]
-     
+        label: "ESTADISTICA",
+        data: [
+          document.getElementById("likes").innerHTML,
+          document.getElementById("dislikes").innerHTML,
+        ],
+        borderColor: "#1808a2",
+        backgroundColor: "#d99e2b",
+      },
+    ],
   },
-  /*
-  plugins:{
-    legend:{position:'left'}
-  }
-  */
-
-},
 });
